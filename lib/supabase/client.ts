dbf4e3752b72691767a9f59e3070c805a9988efc
Supradeep Mukherjee <supradeep2004@gmail.com>
2026-03-12 11:33:54 +0530

    Supabase setup

---
+++ lib/supabase/client.ts
@@ -0,0 +1,5 @@
+import { createBrowserClient } from "@supabase/ssr"
+
+const createClient = () => {
+    return createBrowserClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!)
+}
---
+++ lib/supabase/server.ts
@@ -0,0 +1,18 @@
+import { createServerClient } from "@supabase/ssr"
+import { cookies } from "next/headers"
+
+const createClient = async () => {
+    const cookieStore = await cookies()
+    return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!, {
+        cookies: {
+            getAll() { return cookieStore.getAll() },
+            setAll(cookiesToSet) {
+                try {
+                    cookiesToSet.forEach(({ name, options, value }) => cookieStore.set(name, value, options))
+                } catch (err) {
+                    console.log(err)
+                }
+            }
+        }
+    })
+}
